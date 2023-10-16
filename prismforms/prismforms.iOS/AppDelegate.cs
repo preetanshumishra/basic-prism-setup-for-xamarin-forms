370815e6016bbfcf71ae1149d24e4bb047475feb
Preetanshu Mishra <preetanshumishra@gmail.com>
2023-10-16 19:56:41 -0300

    Updating Xamarin.Forms and Prism
    Updating Xamarin.Forms and Prism

--- prismforms/prismforms.iOS/AppDelegate.cs
+++ prismforms/prismforms.iOS/AppDelegate.cs
@@ -2,6 +2,7 @@
 using Prism;
 using Prism.Ioc;
 using UIKit;
+using Xamarin.Forms.Platform.iOS;
 
 namespace prismforms.iOS
 {
@@ -9,7 +10,7 @@ namespace prismforms.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : FormsApplicationDelegate
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
